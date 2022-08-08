c942d3ff4d4fea9d1952561b98ca1a6438a268b9
drhuy <huydt0307@gmail.com>
2022-08-08 09:02:59 +0700

    add modal

---
+++ assets/js/all.js
@@ -0,0 +1,39 @@
+
+	const loader = document.getElementById('loader');
+
+	const modal = document.getElementById('modal');
+
+(()=>{
+
+	let modal_container = modal.querySelector('.modal-container');
+
+	function add_message(message){
+		modal_container.innerHTML += message + '<br />';
+	}
+
+	app.auth.ajax.onError = function(e){
+		if(!e.message)
+			return;
+		let messages = JSON.parse(e.message);
+		modal.style.display = 'flex';
+		modal_container.innerHTML = '';
+		for(let k in messages){
+			if(typeof messages[k] == 'object')
+				for(let i in messages[k])
+					add_message(messages[k][i]);
+			else
+				add_message(messages[k]);
+		}
+	}
+
+	app.auth.ajax.onBeforeAction = function(){
+		loader.style.display = null;
+		modal.style.display = null;
+		loader.style.display = 'flex';
+	}
+
+	app.auth.ajax.onAfterAction = function(){
+		loader.style.display = null;
+	}
+
+})();
--- assets/js/libs.js
+++ assets/js/libs.js
@@ -1,12 +1,12 @@
 const app = (()=>{
 
 	const PATH = {
-		LOGIN: '/w-admin/',
-		LOGED: '/w-admin/admin.html'
+		LOGIN: '/admin/',
+		LOGED: '/admin/admin.html'
 	}
 	function EventHandle(a,b){let c={};function d(b){Object.defineProperty(a,b,{set:function(a){if("function"==typeof a){let d=a.name||"__";c[b]=c[b]||{},c[b][d]=c[b][d]||[],c[b][d].push(a)}else throw"handle is not a function"}}),Object.defineProperty(a.handle,b,{get:function(){var a;return a=b,Object.freeze({fire(d){for(let e in c[a]){let b=c[a][e];for(let f in b)b[f](d)}},remove(b){b=b||"__",delete c[a][b]},clear(){c[a]={}}})}})}return a.handle={add(a){for(let b in a)d(a[b])},listen(b,c){d(b),a[b]=c}},!function(){for(let a in b)d(b[a])}(),a}
 	function createStorage(a){let c=JSON.parse(localStorage.getItem(a))??{},d=()=>{localStorage.setItem(a,JSON.stringify(c))},e={get:a=>c[a],set(a,b){c[a]=b,d()},remove(a){delete c[a],d()},destroy(){localStorage.removeItem(a)},clear(){c={},d()}},b={};return!function(){for(let a in e)Object.defineProperty(b,a,{get:function(){return e[a]}})}(),b}
-	function Ajax(){let b=["onSuccess","onError"],a={},c,d=new XMLHttpRequest,e={get(a,b={},c=function(){}){g(a,b,"GET",c)},post(a,b={},c=function(){}){g(a,b,"POST",c)},put(a,b={},c=function(){}){g(a,b,"PUT",c)},delete(a,b={},c=function(){}){g(a,b,"DELETE",c)}};function f(c,d){var a,e=[];for(a in c)if(c.hasOwnProperty(a)){var g=d?d+"["+a+"]":a,b=c[a];e.push(null!==b&&"object"==typeof b?f(b,g):encodeURIComponent(g)+"="+encodeURIComponent(b))}return e.join("&")}function g(g,a={},h="GET",i=function(){}){d.onload=function(){if(4==this.readyState&&200==this.status){let a=JSON.parse(this.responseText);i(a),c.onSuccess.fire(a)}else c.onError.fire({status:this.status,message:this.responseText})},d.open(h,g,!0),d.setRequestHeader("Content-type","application/x-www-form-urlencoded");let b=a._header;for(let e in b)d.setRequestHeader(e,b[e]);delete a._header,d.send(f(a))}return!function(){for(let d in c=EventHandle(a,b).handle,e)Object.defineProperty(a,d,{get:function(){return e[d]}})}(),a}
+	function Ajax(){let b=["onBeforeAction","onAfterAction","onSuccess","onError"],a={},c,d=new XMLHttpRequest,e={get(a,b={},c=function(){}){g(a,b,"GET",c)},post(a,b={},c=function(){}){g(a,b,"POST",c)},put(a,b={},c=function(){}){g(a,b,"PUT",c)},delete(a,b={},c=function(){}){g(a,b,"DELETE",c)}};function f(c,d){var a,e=[];for(a in c)if(c.hasOwnProperty(a)){var g=d?d+"["+a+"]":a,b=c[a];e.push(null!==b&&"object"==typeof b?f(b,g):encodeURIComponent(g)+"="+encodeURIComponent(b))}return e.join("&")}function g(g,a={},h="GET",i=function(){}){c.onBeforeAction.fire(),d.onload=function(){if(4==this.readyState&&200==this.status){let a=JSON.parse(this.responseText);i(a),c.onSuccess.fire(a)}else c.onError.fire({status:this.status,message:this.responseText});c.onAfterAction.fire(this)},d.open(h,g,!0),d.setRequestHeader("Content-type","application/x-www-form-urlencoded");let b=a._header;for(let e in b)d.setRequestHeader(e,b[e]);delete a._header,d.send(f(a))}return!function(){for(let d in c=EventHandle(a,b).handle,e)Object.defineProperty(a,d,{get:function(){return e[d]}})}(),a}
 	function Auth(){
 
 		const URI = 'http://localhost/LumenApi/public/api';
@@ -17,7 +17,7 @@ const app = (()=>{
 
 		let profile;
 
-		let self = {profile}
+		let self = {profile, ajax}
 
 		let fns = {
 			login(data, callback = function(){}){
@@ -59,10 +59,6 @@ const app = (()=>{
 			Object.defineProperty(self, 'profile', {
 				get: function(){ return profile; }
 			});
-
-			ajax.onError = function(e){
-				console.dir(e);
-			};
 
 			// if(self.token)
 			// 	self.refresh_token();
